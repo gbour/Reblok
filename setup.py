528b8d19038084238eb51cd4ad34c3fb154c1b06
Guillaume Bour <guillaume@bour.cc>
2010-10-22 22:07:12 +0000

    [feat] add setup.py file
    git-svn-id: http://devedge.bour.cc/svn/reblok/trunk@89 ff29af12-de15-47db-b328-d843b730dfdc

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+__version__ = "$Revision$ $Date$"
+
+from distutils.core import setup
+
+setup(
+	name         = 'reblok',
+	version      = '0.1.0',
+	description  = 'Python decompiler',
+	author       = 'Guillaume Bour',
+	author_email = 'guillaume@bour.cc',
+	url          = 'http://devedge.bour.cc/wiki/Reblok/',
+	license      = 'GPL v3',
+
+	packages=['reblok'],
+	#	data_files('/usr/share/reblok', [
+)
